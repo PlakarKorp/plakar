3e4e8c10fee7c3099174b0b0155ba55a967284ea
Gilles Chehade <gilles@poolp.org>
2025-08-08 13:32:43 +0200

    rework fuse

--- plakarfs/fs.go
+++ plakarfs/fs.go
@@ -3,6 +3,10 @@
 package plakarfs
 
 import (
+	"encoding/binary"
+	"fmt"
+	"hash/fnv"
+
 	"github.com/PlakarKorp/kloset/repository"
 	"github.com/PlakarKorp/plakar/locate"
 	"github.com/anacrolix/fuse/fs"
@@ -12,6 +16,39 @@ type FS struct {
 	repo          *repository.Repository
 	locateOptions *locate.LocateOptions
 	snapshots     []string
+	inodeSeed     uint64
+}
+
+func (fs *FS) initInodeSeed() {
+	if fs.inodeSeed == 0 {
+		// you can make this truly random if you want, or derived from repo UUID
+		fs.inodeSeed = 0x6d5f_3b29_d4e1_9c07
+	}
+}
+
+func (fs *FS) inodeFor(namespace string, parts ...string) uint64 {
+	h := fnv.New64a()
+
+	var seed [8]byte
+	binary.LittleEndian.PutUint64(seed[:], fs.inodeSeed)
+	h.Write(seed[:])
+
+	h.Write([]byte{0})
+	h.Write([]byte(namespace))
+
+	for _, p := range parts {
+		h.Write([]byte{0xff})
+		h.Write([]byte(p))
+	}
+
+	ino := h.Sum64()
+	if ino == 0 || ino == 1 {
+		ino ^= 0x9e3779b97f4a7c15 // mix a constant
+		if ino == 0 || ino == 1 {
+			ino += 2
+		}
+	}
+	return ino
 }
 
 func NewFS(repo *repository.Repository, locateOptions *locate.LocateOptions, snapshots []string, mountpoint string) *FS {
@@ -24,5 +61,6 @@ func NewFS(repo *repository.Repository, locateOptions *locate.LocateOptions, sna
 }
 
 func (f *FS) Root() (fs.Node, error) {
-	return &Dir{name: "/", repo: f.repo, fs: f}, nil
+	fmt.Println("Root() called: Initializing root directory")
+	return &Dir{name: "/", repo: f.repo, fs: f, ino: 1}, nil
 }
