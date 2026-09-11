793778957fc61ce16adfb14fee0f413733a786e8
Gilles Chehade <gilles@poolp.org>
2026-09-11 14:09:33 +0200

    release v1.1.6

--- utils/utils.go
+++ utils/utils.go
@@ -334,7 +334,7 @@ func GetDataDir(appName string) (string, error) {
 }
 
 var VERSION = func() string {
-	version := "v1.1.5"
+	version := "v1.1.6"
 	if strings.HasSuffix(version, "-devel") {
 		if info, ok := debug.ReadBuildInfo(); ok {
 			for _, setting := range info.Settings {
